cc66149726b12422c36f8117d8428547b6b40382
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-11-11 17:29:27 +0100

    fekih_jean-pierre: message parser - skeleton

--- fekih_jean-pierre/src/parser/MessageParser.java
+++ fekih_jean-pierre/src/parser/MessageParser.java
@@ -1,5 +1,92 @@
 package parser;
 
+import java.util.regex.Pattern;
+
 public class MessageParser {
+
+	private String msg;
+	private boolean parsed;
+
+	public MessageParser(String m) {
+
+		if (m == null)
+			throw new NullPointerException("null string");
+
+		if (m.isEmpty())
+			throw new IllegalArgumentException("empty string");
+
+		msg = m;
+		parsed = false;
+		parse();
+	}
+
+	private void parse() {
+
+		Pattern p = Pattern.compile(Keyword.COLON);
+		String[] tokens = p.split(msg);
+
+		switch (tokens[0]) {
+
+		case Keyword.CONNECT:
+			parseConnect(tokens);
+			break;
+
+		case Keyword.CODE:
+			parseCode(tokens);
+			break;
+
+		case Keyword.ANLIST:
+			parseListAnnonce(tokens);
+			break;
+
+		case Keyword.ANNOUNCE:
+			parseAnnonce(tokens);
+			break;
+
+		case Keyword.MSG:
+			parseMessage(tokens);
+			break;
+
+		case Keyword.DISCONNECT:
+			parseDisconnect(tokens);
+			break;
+
+		}
+	}
+
+	private void parseConnect(final String[] tokens) {
+
+		throw new Exception("parseConnect() not implemented yet");
+	}
+
+	private void parseCode(final String[] tokens) {
+
+		throw new Exception("parseCode() not implemented yet");
+	}
+
+	private void parseListAnnonce(final String[] tokens) {
+
+		throw new Exception("parseListAnnonce() not implemented yet");
+	}
+
+	private void parseAnnonce(final String[] tokens) {
+
+		throw new Exception("parseAnnonce() not implemented yet");
+	}
+
+	private void parseMessage(final String[] tokens) {
+
+		throw new Exception("parseMessage() not implemented yet");
+	}
+
+	private void parseDisconnect(final String[] tokens) {
+
+		throw new Exception("parseDisconnect() not implemented yet");
+	}
+
+	public boolean isWellParsed() {
+
+		return parsed;
+	}
 
 }
