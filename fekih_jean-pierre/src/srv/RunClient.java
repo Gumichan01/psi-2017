45dec3d687f2c2684eb3ac9fb2af94649dfb186a
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-11-15 14:02:14 +0100

    runclient: read message

--- fekih_jean-pierre/src/srv/RunClient.java
+++ fekih_jean-pierre/src/srv/RunClient.java
@@ -1,7 +1,16 @@
 package srv;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
+
+import parser.MessageParser;
+
+import com.sun.imageio.plugins.common.InputStreamAdapter;
+import com.sun.xml.internal.bind.v2.TODO;
 
 public class RunClient implements Runnable {
 
@@ -14,9 +23,37 @@ public class RunClient implements Runnable {
 
 	public void run() {
 
-		System.out.println("OK");
+		boolean keep_going = true;
+		String received_message;
+		MessageParser parser;
+
 
 		try {
+
+			BufferedReader bf = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
+			
+			System.out.println("Connection from " + sock.getInetAddress().toString() + ": " + sock.getPort());
+			
+			while (keep_going) {
+				
+				received_message = bf.readLine();
+				
+				if(received_message == null || received_message.isEmpty()) {
+					
+					keep_going = false;
+					sock.close();
+					break;
+				}
+
+				parser = new MessageParser(received_message);
+				
+				if(parser.isWellParsed()) {
+					
+					/// todo RECUPÉRER MESSAGE
+				}
+				
+			}
 
 			sock.close();
 
