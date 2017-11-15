777f39279d0e642f6735b3f7805a3abf923bcc40
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-11-15 13:12:29 +0100

    parser: fix

--- fekih_jean-pierre/src/parser/MessageParser.java
+++ fekih_jean-pierre/src/parser/MessageParser.java
@@ -25,61 +25,73 @@ public class MessageParser {
 		Pattern p = Pattern.compile(Keyword.COLON);
 		String[] tokens = p.split(msg);
 
-		switch (tokens[0]) {
+		try {
+			
+			switch (tokens[0]) {
 
-		case Keyword.CONNECT:
-			parseConnect(tokens);
-			break;
+			case Keyword.CONNECT:
+				parseConnect(tokens);
+				break;
 
-		case Keyword.CODE:
-			parseCode(tokens);
-			break;
+			case Keyword.CODE:
+				parseCode(tokens);
+				break;
 
-		case Keyword.ANLIST:
-			parseListAnnonce(tokens);
-			break;
+			case Keyword.ANLIST:
+				parseListAnnonce(tokens);
+				break;
 
-		case Keyword.ANNOUNCE:
-			parseAnnonce(tokens);
-			break;
+			case Keyword.ANNOUNCE:
+				parseAnnonce(tokens);
+				break;
 
-		case Keyword.MSG:
-			parseMessage(tokens);
-			break;
+			case Keyword.MSG:
+				parseMessage(tokens);
+				break;
 
-		case Keyword.DISCONNECT:
-			parseDisconnect(tokens);
-			break;
+			case Keyword.DISCONNECT:
+				parseDisconnect(tokens);
+				break;
 
+			default:
+				parsed = false;
+				break;
+			}
+			
+		} catch (Exception e) {
+			
+			parsed = false;
+			e.printStackTrace();
 		}
+
 	}
 
-	private void parseConnect(final String[] tokens) {
+	private void parseConnect(final String[] tokens) throws Exception {
 
 		throw new Exception("parseConnect() not implemented yet");
 	}
 
-	private void parseCode(final String[] tokens) {
+	private void parseCode(final String[] tokens) throws Exception {
 
 		throw new Exception("parseCode() not implemented yet");
 	}
 
-	private void parseListAnnonce(final String[] tokens) {
+	private void parseListAnnonce(final String[] tokens) throws Exception {
 
 		throw new Exception("parseListAnnonce() not implemented yet");
 	}
 
-	private void parseAnnonce(final String[] tokens) {
+	private void parseAnnonce(final String[] tokens) throws Exception {
 
 		throw new Exception("parseAnnonce() not implemented yet");
 	}
 
-	private void parseMessage(final String[] tokens) {
+	private void parseMessage(final String[] tokens) throws Exception {
 
 		throw new Exception("parseMessage() not implemented yet");
 	}
 
-	private void parseDisconnect(final String[] tokens) {
+	private void parseDisconnect(final String[] tokens) throws Exception {
 
 		throw new Exception("parseDisconnect() not implemented yet");
 	}
