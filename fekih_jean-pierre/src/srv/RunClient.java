64fa3b75745074b46df7563d6ad93eabdbf1a6ab
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-11-16 11:48:43 +0100

    run client: format

--- fekih_jean-pierre/src/srv/RunClient.java
+++ fekih_jean-pierre/src/srv/RunClient.java
@@ -9,9 +9,6 @@ import java.net.Socket;
 
 import parser.MessageParser;
 
-import com.sun.imageio.plugins.common.InputStreamAdapter;
-import com.sun.xml.internal.bind.v2.TODO;
-
 public class RunClient implements Runnable {
 
 	private Socket sock;
@@ -27,41 +24,49 @@ public class RunClient implements Runnable {
 		String received_message;
 		MessageParser parser;
 
-
 		try {
 
-			BufferedReader bf = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-			PrintWriter pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
-			
-			System.out.println("Connection from " + sock.getInetAddress().toString() + ": " + sock.getPort());
-			
+			BufferedReader bf = new BufferedReader(new InputStreamReader(
+					sock.getInputStream()));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(
+					sock.getOutputStream()));
+
+			System.out.println("Connection from "
+					+ sock.getInetAddress().toString() + ": " + sock.getPort());
+
 			while (keep_going) {
-				
+
 				received_message = bf.readLine();
-				
-				if(received_message == null || received_message.isEmpty()) {
-					
+
+				if (received_message == null || received_message.isEmpty()) {
+
 					keep_going = false;
 					sock.close();
 					break;
 				}
 
 				parser = new MessageParser(received_message);
-				
-				if(parser.isWellParsed()) {
-					
-					/// todo RECUPÉRER MESSAGE
+
+				if (parser.isWellParsed()) {
+
+					// TODO récupérer la repésentation abstraite du message
+					// TODO traiter la demande analysée
+					// TODO envoyer la réponse au client
+
+				} else {
+
+					// TODO envoi message d'erreur
 				}
-				
+
+				keep_going = false; // pour tester (à enlever)
 			}
 
 			sock.close();
 
 		} catch (IOException ie) {
 			ie.printStackTrace();
-		} catch (NullPointerException ne) {
+		} catch (NullPointerException | IllegalArgumentException ne) {
 			ne.printStackTrace();
 		}
 	}
-
 }
