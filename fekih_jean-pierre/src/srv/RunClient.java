8924e9cad656b8508e243a76faf5b35cabc1e892
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-11-22 16:07:47 +0100

    run client: announces

--- fekih_jean-pierre/src/srv/RunClient.java
+++ fekih_jean-pierre/src/srv/RunClient.java
@@ -8,10 +8,12 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
+import data.AnnounceData;
 import data.ClientData;
 
 import parser.ASTmessage;
 import parser.ASTmessage.Type;
+import parser.Keyword;
 import parser.MessageParser;
 
 public class RunClient implements Runnable {
@@ -20,7 +22,7 @@ public class RunClient implements Runnable {
 	private BufferedReader bf = null;
 	private PrintWriter pw = null;
 	private int client_id;
-	
+
 	public RunClient(Socket s) {
 
 		sock = s;
@@ -76,46 +78,47 @@ public class RunClient implements Runnable {
 		} finally {
 			try {
 				sock.close();
-			} catch (IOException e) {}
+			} catch (IOException e) {
+			}
 		}
 	}
 
 	private String eval(final ASTmessage ast) {
 
 		String s = null;
-		
-		switch(ast.getType()){
+
+		switch (ast.getType()) {
 
 		// add announce
 		case ANNOUNCE:
 			s = evalAddAnnounce(ast);
 			break;
-		
+
 		// get the owner of an announce
 		case COM:
 			s = evalGetOwner(ast);
 			break;
-			
+
 		// connection
 		case CONNECT:
 			s = evalConnection(ast);
 			break;
-		
+
 		// remove an announce
 		case DEL:
 			s = evalDelAnnounce(ast);
 			break;
-		
+
 		// disconnection
 		case DISCONNECT:
 			s = evalDisconnection(ast);
 			break;
-		
+
 		// Get announce
 		case GET:
 			s = evalGetAnnounce(ast);
 			break;
-			
+
 		// display list of announces
 		case LIST:
 			s = evalList(ast);
@@ -124,65 +127,74 @@ public class RunClient implements Runnable {
 		default:
 			break;
 		}
-		
+
 		return s;
 	}
 
 	// connect
 	private String evalConnection(final ASTmessage ast) {
-		
-		int port        = ast.getConnect().getPort();
-		InetAddress ine = sock.getInetAddress(); 
-		
+
+		int port = ast.getConnect().getPort();
+		InetAddress ine = sock.getInetAddress();
+
 		ClientData cdata = new ClientData(ine, port);
 		client_id = cdata.getId();
-		
+
 		Server.clients.add(cdata);
-		return "code:con:OK\n";
+		return Keyword.CODE + Keyword.COLON + Keyword.CON + Keyword.COLON
+				+ Keyword.SUCCESS + Keyword.ENDL;
 	}
 
 	// disconnect
 	private String evalDisconnection(final ASTmessage ast) {
- 		
+
 		Server.clients.delete(client_id);
 		return null;
 	}
-	
+
 	// list
 	private String evalList(final ASTmessage ast) {
-		
+
 		return null;
 	}
 
 	// add announce
 	private String evalAddAnnounce(final ASTmessage ast) {
-		
+
 		return null;
 	}
 
 	private String evalDelAnnounce(final ASTmessage ast) {
-		
+
 		return null;
 	}
-	
+
 	// get announce
 	private String evalGetAnnounce(final ASTmessage ast) {
-		
-		return null;
+
+		int id = ast.getAnnounceID().getId();
+		AnnounceData data = Server.announces.getAnnounce(id);
+
+		if (data == null)
+			return Keyword.CODE + Keyword.COLON + Keyword.FAILURE
+					+ Keyword.ENDL;
+
+		return Keyword.ANNOUNCE + Keyword.COLON + data.toString()
+				+ Keyword.ENDL;
 	}
-	
+
 	// get client
 	private String evalGetOwner(final ASTmessage ast) {
-		
+
 		return null;
 	}
-	
-	
+
 	private void respond(String msg) {
-		
-		if(msg == null)
+
+		if (msg == null)
 			return;
-		
+
+		System.out.println(msg);
 		pw.println(msg);
 		pw.flush();
 	}
