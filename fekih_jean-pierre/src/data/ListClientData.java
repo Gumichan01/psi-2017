2e5a6298e02edef08583fa6359c28026390be4ba
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-12-07 11:45:08 +0100

    data: update clients - synchronized

--- fekih_jean-pierre/src/data/ListClientData.java
+++ fekih_jean-pierre/src/data/ListClientData.java
@@ -4,46 +4,62 @@ import java.util.HashMap;
 
 public class ListClientData {
 
-	private HashMap<Integer, ClientData> map;
+	private volatile HashMap<Integer, ClientData> map;
 
 	public ListClientData() {
 		map = new HashMap<>();
 
 	}
 
-	synchronized public boolean add(ClientData client) {
+	public boolean add(ClientData client) {
 
-		if (!map.containsKey(client.getId())) {
+		synchronized (map) {
 
-			map.put(client.getId(), client);
-			return true;
+			if (!map.containsKey(client.getId())) {
+
+				map.put(client.getId(), client);
+				return true;
+			}
 		}
 
 		return false;
 	}
 
-	synchronized public boolean exists(int id) {
+	public boolean exists(int id) {
 
-		return map.containsKey(id);
-	}
+		boolean b = false;
+		;
 
-	synchronized public ClientData get(int id) {
-
-		if (!map.containsKey(id)) {
-
-			return null;
+		synchronized (map) {
+			b = map.containsKey(id);
 		}
 
-		return map.get(id);
+		return b;
 	}
 
-	synchronized public boolean delete(int cid) {
+	public ClientData get(int id) {
 
-		if (map.containsKey(cid)) {
-			map.remove(cid);
-			return true;
+		synchronized (map) {
+
+			if (!map.containsKey(id)) {
+
+				return null;
+			}
+
+			return map.get(id);
 		}
+	}
 
-		return false;
+	public boolean delete(int cid) {
+
+		synchronized (map) {
+			
+			if (map.containsKey(cid)) {
+				map.remove(cid);
+				return true;
+			}
+
+			return false;
+		}
 	}
 }
